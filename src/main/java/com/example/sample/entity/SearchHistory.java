f4e37853e6eedb80eb2a72a1c16c36d610a3e429
ysleewrk <yslee.wrk@gmail.com>
2023-03-20 17:48:03 +0900

    commit;

--- src/main/java/com/example/sample/entity/SearchHistory.java
+++ src/main/java/com/example/sample/entity/SearchHistory.java
@@ -2,20 +2,32 @@ package com.example.sample.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "TB_SEARCH_HISTORY")
-public class SearchHistory {
+public class SearchHistory extends BaseEntity {
     @Id
     @GeneratedValue
     private Long id;
 
     @Column
     private String platform;
+
+    @Column
+    private String keyword;
+
+    @Column
+    private String deviceId;
+
+
 }
 
 
